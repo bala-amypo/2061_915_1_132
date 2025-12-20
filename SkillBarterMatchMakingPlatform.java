119f5233519669f1b9803e74caf2877ab2eebd0b
bala-amypo <bala@amypo.in>
2025-12-20 21:03:08 +0000

    User commit

---
+++ SkillBarterMatchMakingPlatform.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import com.example.barter.controller.*;
+import com.example.barter.dto.*;
+import com.example.barter.model.*;
+import com.example.barter.security.*;
+import com.example.barter.service.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
