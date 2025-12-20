a09998c2cc633c08960636c912b9ddb11d1baebe
bala-amypo <bala@amypo.in>
2025-12-20 20:44:24 +0000

    User commit

--- src/main/java/com/example/barter/config/OpenApiConfig.java
+++ src/main/java/com/example/barter/config/OpenApiConfig.java
@@ -1,4 +1,14 @@
-package com.example.barter.config;
+package com.example.demo;
+
+import com.example.barter.controller.*;
+import com.example.barter.dto.*;
+import com.example.barter.model.*;
+import com.example.barter.security.*;
+import com.example.barter.service.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
