10923e7e31cac99640c8e12d1548de54babcae24
bala-amypo <bala@amypo.in>
2025-12-20 17:17:16 +0000

    User commit

--- src/main/java/com/example/barter/dto/AuthRequest.java
+++ src/main/java/com/example/barter/dto/AuthRequest.java
@@ -1,2 +1,12 @@
-package com.example.demo.dto;
-public 
+package com.example.barter.dto;
+
+public class AuthRequest {
+    private String email;
+    private String password;
+
+    public AuthRequest() {}
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
