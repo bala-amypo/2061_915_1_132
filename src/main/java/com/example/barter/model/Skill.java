022f66a1c1e9e477d880e1943e745d275496fb2e
bala-amypo <bala@amypo.in>
2025-12-20 19:41:12 +0000

    User commit

---
+++ src/main/java/com/example/barter/model/Skill.java
@@ -0,0 +1,16 @@
+package com.example.barter.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Skill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
