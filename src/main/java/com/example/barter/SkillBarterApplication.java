6f676fa5f1902a25e8c1ae7135a33ae05376dd0b
bala-amypo <bala@amypo.in>
2025-12-20 21:01:43 +0000

    User commit

--- src/main/java/com/example/barter/SkillBarterApplication.java
+++ src/main/java/com/example/barter/SkillBarterApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SkillBarterApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SkillBarterApplication.class, args);
     }
 }
-//SkillApplication
