2ef78985c6a3b8c8aa3ff9c67f86fa11145c3613
bala-amypo <bala@amypo.in>
2025-12-20 19:15:58 +0000

    User commit

---
+++ src/main/java/com/example/barter/listener/TestResultListener.java
@@ -0,0 +1,38 @@
+package com.example.barter.listener;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.ITestContext;
+
+public class TestResultListener implements ITestListener {
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        // Function declaration only 
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        // Function declaration only 
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        // Function declaration only 
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        // Function declaration only 
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+        // Function declaration only 
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        // Function declaration only 
+    }
+}
