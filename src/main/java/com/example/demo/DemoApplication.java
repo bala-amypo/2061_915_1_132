df32e9d5ce104c2d4dde8d3c92141270d349cabb
bala-amypo <bala@amypo.in>
2025-12-21 07:42:54 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-public class SkillBarterApplication {
+@ServletComponentScan // Required for the SimpleStatusServlet
+public class DemoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SkillBarterApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
