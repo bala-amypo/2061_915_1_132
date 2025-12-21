6b98241252eae9694f2231cfb89f8c154951c383
bala-amypo <bala@amypo.in>
2025-12-21 08:03:54 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+    
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    private String role = "MONITOR"; // Default as per requirements
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
